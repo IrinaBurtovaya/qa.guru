29cfd4e44eefac0b75ad438071f3255f342cc00e
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-03-30 23:19:57 +0300

    Added new upper case  line!

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My first code in the repository");
-        System.out.println("This is the second line in my program");
+        System.out.println("This is upper case");
     }
 }
