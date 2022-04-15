d5d66439c021764149da8503f562318430a96280
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-04-15 10:58:42 +0300

    created new branch ArithmeticOperations, moved from old repo

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.sql.SQLOutput;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("My first code in the repository");
-        System.out.println("THIS IS UPPER CASE");
-    }
-}
