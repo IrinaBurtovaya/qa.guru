31dee1207e9ebe28d1876cfc6a84f509a6690ffc
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-04-15 11:47:25 +0300

    MainUpperCaseBranch updated

--- src/main/java/MainUpperCaseBranch.java
+++ src/main/java/MainUpperCaseBranch.java
@@ -1,9 +1,10 @@
-import java.sql.SQLOutput;
-
-public class Main {
+public class MainUpperCaseBranch {
 
     public static void main(String[] args) {
         System.out.println("My first code in the repository");
         System.out.println("THIS IS UPPER CASE");
     }
 }
+
+
+
