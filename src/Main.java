cb11c785d51c9289d58b9fb4602bee0254002941
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-04-15 11:14:01 +0300

    update new branch

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.sql.SQLOutput;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("My first code in the repository");
-        System.out.println("This is the second line in my program");
-    }
-}
